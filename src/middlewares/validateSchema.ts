686f31db7e41f611fc16287eab934d522c8d00f3
Pedro-Nunes22 <pedrosoaresnunes2003@gmail.com>
2026-04-19 17:31:14 -0300

    feat(middleware): handle validation errors in validateSchema

--- src/middlewares/validateSchema.ts
+++ src/middlewares/validateSchema.ts
@@ -1,13 +1,25 @@
 import { Response, Request, NextFunction } from "express";
-import { ZodType } from "zod";
+import { ZodType, ZodError } from "zod";
 
 export const validateSchema = (schema: ZodType) => async (req: Request, res: Response, next: NextFunction) => {
     try {
         await schema.parseAsync({
-
+            body: req.body,
+            query: req.query,
+            params: req.params,
         })
-        next();
+        return next();
     } catch (error) {
-        
+        if(error instanceof ZodError) {
+            return res.status(400).json ({
+                error: "validation error",
+                details: error.issues.map( issue => ({
+                    mensagem: issue.message
+                }))
+            })
+        }
+        return res.status(500).json({
+            error: "Internal server error"
+        })
     }
 }
